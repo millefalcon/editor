97908f46b2f5ae7d2b47950f1078da813c08abe2
hanish <hanish0019@gmail.com>
2025-02-01 11:37:23 -0500

    Keep

---
+++ app/editor/page.tsx
@@ -0,0 +1,11 @@
+// "use client";
+
+import PlaygroundApp from "@/components/editor/src/App";
+
+export default function Page() {
+  return (
+    <div className="h-screen w-full" data-registry="plate">
+      <PlaygroundApp />
+    </div>
+  );
+}
